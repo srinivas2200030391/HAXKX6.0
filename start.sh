6aa88d6d1760a82acb89bf1c2c6d545e7e48b49d
renu-aayush-maddi <2200032294@kluniversity.in>
2025-07-23 10:36:56 +0530

    okk

--- start.sh
+++ start.sh
@@ -1,3 +1,9 @@
-#!/bin/bash
+echo "Setting up Conda..."
+conda env create -f environment.yaml || true
+source /opt/conda/etc/profile.d/conda.sh
 conda activate chatbotenv
+
+echo "Starting server..."
+pip uninstall -y pinecone-plugin-inference
+
 gunicorn --bind 0.0.0.0:10000 wsgi:app
